c7fc99458bd1551da855079cb52cd91e28c76471
James Grant <jcg13@ic.ac.uk>
2015-11-13 14:56:48 +0000

    Began adding Flask SQLAlchemy Database models

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
 from app import views, models, socketio_server
