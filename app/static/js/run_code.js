4e11b83ebce50a61d613648b98a82d24accda4eb
Alexandru <an2013@ic.ac.uk>
2015-11-14 07:53:17 -0800

    Added visual output from python

--- app/static/js/run_code.js
+++ app/static/js/run_code.js
@@ -1,8 +1,7 @@
 function runJScode() {
     var js = editor.getValue();
-    var s = document.createElement('script');
-    s.textContent = js; 
-    document.body.appendChild(s);
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = js;
 }
 
 function runPYcode() {
@@ -10,6 +9,36 @@ function runPYcode() {
     pypyjs.exec(py);
 }
 
+function outf(text) {
+    var mypre = document.getElementById("output"); 
+    mypre.innerHTML = mypre.innerHTML + text; 
+} 
+
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+function runit() { 
+   var prog = editor.getValue();
+   var mypre = document.getElementById("output"); 
+   mypre.innerHTML = ''; 
+   Sk.pre = "output";
+   Sk.configure({output:outf, read:builtinRead}); 
+   (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+   var myPromise = Sk.misceval.asyncToPromise(function() {
+       return Sk.importMainWithBody("<stdin>", false, prog, true);
+   });
+   myPromise.then(function(mod) {
+       console.log('success');
+   },
+       function(err) {
+       console.log(err.toString());
+   });
+} 
+
+$("#clickSkulpt").click(runit);
 $("#clickJs").click(runJScode);
 $("#clickPy").click(runPYcode);
 
