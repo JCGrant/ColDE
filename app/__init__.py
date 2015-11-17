9c7b08e933bc4addb8b8d43ddb3e5fc0215e5bee
James Grant <jcg13@ic.ac.uk>
2015-11-17 15:05:08 +0000

    Added config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from app import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+app.config.from_object(config)
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
---
+++ app/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'super-secret!'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////test.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
