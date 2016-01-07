7a400ece660c8ef86c3f2da2a5d35924c9fcf04d
Stefan <sc7213@doc.ic.ac.uk>
2016-01-07 12:59:11 +0000

    Password is now hidden

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
