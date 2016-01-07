d38922aae0df54fcdaa577f7df1af21af05fa4f3
James Grant <jcg13@ic.ac.uk>
2016-01-07 21:22:08 +0000

    Move navbar user experience code to project.js.

--- app/static/js/codemirror.js
+++ app/static/js/codemirror.js
@@ -383,45 +383,6 @@ function runit() {
    });
 }
 
-webViewOpen = false;
-var webViewToggleButton = $("#toggleWebView");
-
-function toggleWebView() {
-  if (webViewOpen) {
-    closeConsole();
-  } else {
-    showConsole();
-  }
-  webViewOpen = !webViewOpen;
-  var webVIewToggleButtonText = (webViewOpen ? 'Close' : 'Open') + ' Web View ' + '<span class="glyphicon glyphicon-modal-window"></span>';
-  webViewToggleButton.html(webVIewToggleButtonText);
-}
-
-function showConsole() {
-  if(document.getElementById("webview") == null) {
-    var editorview = document.getElementById("editorview");
-    var frameview = document.getElementById("frameview");
-    editorview.className = "col-md-5";
-    var frame = document.createElement("iframe");
-    frame.className = "col-md-5";
-    frame.scrolling = "yes";
-    // frame.sandbox = "allow-same-origin allow-scripts allow-popups allow-forms";
-    frame.id = "webview";
-    frameview.appendChild(frame);
-  }
-}
-
-function closeConsole() {
-  if(document.getElementById("webview") != null) {
-    var editorview = document.getElementById("editorview");
-    var frameview = document.getElementById("frameview");
-    var child = document.getElementById("webview");
-    frameview.removeChild(webview);
-    editorview.className = "col-md-10";
-  }
-} 
-
-
 function preprocess(text, type, filelist) {
   if(type === 1) {
     var regex = new RegExp('<[\\s\\t]*script.*src[\\s\\t]*=[^>]*>', 'gi');
