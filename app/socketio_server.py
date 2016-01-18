538a8edf4eb19a4344f564161b74bd89a36cb441
Mihai Popa <mihai.popa1201@gmail.com>
2016-01-18 18:51:55 +0000

    Removed some server logs.

--- app/socketio_server.py
+++ app/socketio_server.py
@@ -38,7 +38,6 @@ def clientDisconnect(pads):
 @socketio.on('client_server_changeset')
 def handle(changeset):
     print ('Received ' + str(changeset))
-    print (len(changeset['charBank']))
     # Fetch project and pad ids.
     project_id = changeset['projectId']
     pad_id = changeset['padId']
@@ -111,7 +110,6 @@ def updateDBPad(changeset, crtRev):
         return
     # Update pad text content.
     pad.text = applyChangeset(pad.text, changeset)
-    print ('new text is ' + pad.text)
     # Update code of last revision.
     pad.last_revision = crtRev
     # Write to DB.
@@ -119,8 +117,6 @@ def updateDBPad(changeset, crtRev):
     db.session.commit()
     pad = Pad.query.filter_by(project_id=changeset['projectId']).\
         filter_by(id=changeset['padId']).first()
-    print ('new new text is ' + pad.text)
-    print ('new new text is ' + str(len(pad.text)))
 
 # Handles file manipulations from a client, broadcasts to the others.
 def onFileManipulation(type, content):
